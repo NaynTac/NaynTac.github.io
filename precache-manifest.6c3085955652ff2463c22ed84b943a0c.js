9c8dc1da25ec605248ed53d3e44989f64f4a94e3
mipka <111595895+NaynTac@users.noreply.github.com>
2025-12-17 18:14:18 +0300

    Add files via upload

---
+++ precache-manifest.6c3085955652ff2463c22ed84b943a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b9e7c07cda1b8207cea73262243fd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af88ab654f95ab261b5",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "3af88ab654f95ab261b5",
+    "url": "/static/js/main.46d236b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
